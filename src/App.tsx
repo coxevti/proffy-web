93a55a7c5ced2c64de0a562c43199a6b04db20da
Valdir Coxev [Job] <valcox.russo@gmail.com>
2020-08-05 16:20:23 -0400

    Adicionando routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Landing from './pages/Landing';
 import GlobalStyled from './styles/global';
+import Routes from './routes';
 
 const App: React.FC = () => (
   <>
     <GlobalStyled />
-    <Landing />
+    <Routes />
   </>
 );
 
