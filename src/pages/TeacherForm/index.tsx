428002049db3bced5933d9e0c13c7d8dbcc3e646
Valdir Coxev [Job] <valcox.russo@gmail.com>
2020-08-05 16:25:30 -0400

    Adicionando pagina novo professor

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import PageHeader from '../../components/PageHeader';
+
+import { PageTeacherForm } from './styles';
+
+const TeacherForm: React.FC = () => (
+  <PageTeacherForm>
+    <PageHeader title="Que incrível que você quer dar aulas." />
+  </PageTeacherForm>
+);
+
+export default TeacherForm;
